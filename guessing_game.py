cdc64af8302cfb3c493c6d2b224256b79b9177b2
Kidist A <kidi.amanuel@gmail.com>
2018-10-28 13:27:28 -0500

    Update few lines of code
    - added comments at beginning of file
    - moved line 12 menu() to line 64 and moved current line 64 to 65
    - specified type of exception (ValueError) in line 33 and removed the else block nested under the try

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,6 @@
+# Python Techdegree - Project 1 - Number Guessing Game
+# Aiming for 'exceed' ratings
+
 import random
 
 guesses = []
@@ -9,7 +12,6 @@ def menu():
     Enter 'Help' for game instructions.
     """)
     
-menu()
 
 def help():
     print("""
@@ -31,34 +33,35 @@ def start_game():
         else:
             try:
                 guess = int(guess)
-            except Exception:
+            except ValueError:
                 print("Oops, try again. Make sure to enter a whole number.")
-            else:
-                if guess == number:
-                    count += 1
-                    guesses.append(count)
-                    print("You got it! It took you {} tries.".format(count))
-                    replay = input("Do you want to play again?(Y/N)   ")
-                    if replay.upper() == 'Y':
-                        print("Highest score is {}.".format(min(guesses)))
-                        number = random.randint(1,10)
-                        count = 0
-                        continue
-                    else:                        
-                        print("Game over! See you next time.")
-                        break
-                elif guess <1 or guess > 10: 
-                    print("Not a valid option - make sure to enter a number between 1 and 10.")
-                    continue 
-                elif guess > number:
-                    count += 1
-                    print("Try again - number is lower.")
+                continue
+            if guess == number:
+                count += 1
+                guesses.append(count)
+                print("You got it! It took you {} tries.".format(count))
+                replay = input("Do you want to play again?(Y/N)   ")
+                if replay.upper() == 'Y' or replay.upper() == 'YES':
+                    print("Highest score is {}.".format(min(guesses)))
+                    number = random.randint(1,10)
+                    count = 0
                     continue
-                elif guess < number:
-                    count += 1
-                    print("Try again - number is higher.")
-                    continue 
+                else:                        
+                    print("Game over! See you next time.")
+                    break
+            elif guess <1 or guess > 10: 
+                print("Not a valid option - make sure to enter a number between 1 and 10.")
+                continue 
+            elif guess > number:
+                count += 1
+                print("Try again - number is lower.")
+                continue
+            elif guess < number:
+                count += 1
+                print("Try again - number is higher.")
+                continue 
 
 
 if __name__ == '__main__':
+    menu()
     start_game()
